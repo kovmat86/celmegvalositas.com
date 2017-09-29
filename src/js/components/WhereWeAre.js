dc45928a6e7b2b31433615693fdd32ef5dbad1da
Bence Varga <vbence86@gmail.com>
2017-09-29 15:03:39 +0200

    add GoogleMap and async loader

--- src/js/components/WhereWeAre.js
+++ src/js/components/WhereWeAre.js
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react'
+import scriptLoader from 'react-async-script-loader';
+
+const GoogleMapAPIKey = '';
 
 class WhereWeAre extends React.Component {
+
+  componentWillReceiveProps ({ isScriptLoaded, isScriptLoadSucceed }) {
+    if (isScriptLoaded && !this.props.isScriptLoaded) { // load finished 
+      if (isScriptLoadSucceed) {
+        this.initMap()
+      }
+      else this.props.onError()
+    }
+  }
+ 
+  componentDidMount () {
+    const { isScriptLoaded, isScriptLoadSucceed } = this.props
+    if (isScriptLoaded && isScriptLoadSucceed) {
+      this.initMap()
+    }
+  }
+
+  initMap() {
+
+  }
 
   render() {
     return null;
@@ -8,5 +31,9 @@ class WhereWeAre extends React.Component {
 
 }
 
-export { WhereWeAre };
-export default WhereWeAre;
+const loader = scriptLoader(
+  `https://maps.googleapis.com/maps/api/js?key=${GoogleMapAPIKey}`
+)(WhereWeAre);
+
+export { loader as WhereWeAre};
+export default loader;
