bbe75ac44f14f49610f94634cd44eec6fe5e9a33
Bence Varga <vbence86@gmail.com>
2017-10-04 14:13:33 +0200

    fix missing loader for production builds

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,10 @@ module.exports = {
         loader: 'url-loader',
         query: { mimetype: "image/png" }
       },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },      
       { 
         test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/, 
         loader: 'file-loader'
