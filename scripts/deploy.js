4454881e9005738252682a5bf669522d81ef23e9
Bence Varga <vbence86@gmail.com>
2017-10-04 16:08:20 +0200

    update deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -9,6 +9,9 @@ var production = '/public_html/';
 var staging = '/staging.celmegvalositas.com/';
 var remoteRoot = argv.target === 'production' ? production : staging;
 
+console.log(argv);
+return;
+
 var config = {
   username: process.env.CELMEGVALOSITAS_FTP_USERNAME,
   password: process.env.CELMEGVALOSITAS_FTP_PASSWORD,
