e01ccb2085b56b42703a590a5573018cf9a6e1d8
Bence Varga <vbence86@gmail.com>
2017-11-02 11:27:35 +0100

    why choose us section refactor

--- src/js/components/WhyChooseUs.js
+++ src/js/components/WhyChooseUs.js
@@ -14,20 +14,13 @@ class WhyChooseUs extends React.Component {
 
     return (
       <div className={_className} id="whychooseus">
-        <div className="row">
-          <div className="col-xs-12 col-md-6">
-            <div id="why-choose-us-content-container">
-              <h2 className="animated fadeInDown viewport-animation animated fadeInLeft">{this.state.title}</h2>
-              <p className="animated fadeInDown viewport-animation animated fadeInLeft delay-100">{this.state.text}</p>
-            </div>
-          </div>
-          <div className="col-xs-12 col-md-6">
-            <div id="why-choose-us-items-container" className="container animated fadeIn">
-              <ul>
-                {this.renderItems()}
-              </ul>
-            </div>
-          </div>
+        <div id="why-choose-us-content-container">
+          <h2 className="animated fadeInDown viewport-animation animated fadeInLeft">{this.state.title}</h2>
+          <p className="animated fadeInDown viewport-animation animated fadeInLeft delay-100">{this.state.text}</p>
+        </div>
+        <div id="why-choose-us-items-container" className="row animated fadeIn">
+          {this.renderItems()}
+          {this.renderLastItem()}
         </div>
       </div>
     );
@@ -35,11 +28,40 @@ class WhyChooseUs extends React.Component {
 
   renderItems() {
     if (!this.state.items) return null;
-    return this.state.items.map((v, idx) => (
-      <li key={idx} className="viewport-animation animated fadeInUp delay-{(idx+1) * 250}">
-        <Icon name="user" className="why-choose-us-icon" />
-        <span>{v.title}</span>
-      </li>
+    const itemCount = this.state.items.length;
+    return this.state.items
+      .filter((v, idx) => idx < itemCount - 1)
+      .map((v, idx) => {
+        const delay = (idx+1) * 250;
+        return <div key={idx} className={`col-xs-12 col-md-4 viewport-animation animated fadeInUp delay-${delay}`}>
+          <div className="why-choose-us-item-container">
+            <Icon name={v.icon} className="why-choose-us-icon" />
+            <ul>{this.renderWhyElements(v.listItem)}</ul>
+          </div>
+        </div>
+      });
+  }
+
+  renderLastItem() {
+    const itemIdx = this.state.items.length - 1;
+    const item = this.state.items[itemIdx];
+    return (
+      <div className="container viewport-animation animated fadeInUp">
+        <p>{item.title}</p>
+        <div className="col-xs-12 col-md-12">
+          <div className="why-choose-us-item-container last">
+            <Icon name={item.icon} className="why-choose-us-icon" />
+            <ul>{this.renderWhyElements(item.listItem)}</ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  renderWhyElements(elm) {
+    if (!elm || !elm.length) return null;
+    return elm.map((v, idx) => (
+      <li key={idx}>{v}</li>
     ));
   }
 
