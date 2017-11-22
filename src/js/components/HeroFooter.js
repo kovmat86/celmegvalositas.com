b696741c0bcf350da5bfcb6db4c888ba64df7068
Bence Varga <vbence86@gmail.com>
2017-11-22 15:19:07 +0100

    make HeroFooter to open appointment popup

--- src/js/components/HeroFooter.js
+++ src/js/components/HeroFooter.js
@@ -14,7 +14,7 @@ class HeroFooter extends React.Component {
         <button 
           aria-label={this.state.ctaLabel} 
           className="btn outline btn-white btn-block viewport-animation animated fadeInUp"
-          data-toggle="modal" data-target="#request-phoneback-modal"
+          data-toggle="modal" data-target="#request-appointment-modal"
           >{this.state.ctaLabel}</button>
       </div>
     );
